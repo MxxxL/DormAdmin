2510deac96d7c341a776eace06f9fcf2fd0699d2
MxxxL <49240218+MxxxL@users.noreply.github.com>
2021-06-22 17:10:01 +0800

    dorm-admin

--- dorm-admin-page/src/views/Home.vue
+++ dorm-admin-page/src/views/Home.vue
@@ -1,72 +1,69 @@
 <template>
-    <div>
-        <el-container style="height: 500px; border: 1px solid #eee">
-            <el-header>
-                <div>广软宿舍管理系统</div>
-                <div>
-                    <el-dropdown
-                        @command="commandHandler"
-                    >
-                        <span class="el-dropdown-link">
-                            {{user.name}}
-                        </span>
-                        <el-dropdown-menu slot="dropdown">
-                            <el-dropdown-item command="profile">个人中心</el-dropdown-item>
-                            <el-dropdown-item
-                                command="logout"
-                                divided
-                            >注销登录</el-dropdown-item>
-                        </el-dropdown-menu>
-                    </el-dropdown>
-                </div>
-            </el-header>
-            <el-container>
-                <el-aside width="200px">
-                    <el-menu
-                        router
-                        unique-opened
-                    >
-                        <el-submenu
-                            :index="index+''"
-                            v-for="(item,index) in routes"
-                            v-if="!item.hidden"
-                            :key="index"
-                        >
-                            <template slot="title">
-                                <i
-                                    style="color: #409eff;margin-right: 5px"
-                                    :class="item.iconCls"
-                                ></i>
-                                <span>{{item.name}}</span>
-                            </template>
-                            <el-menu-item
-                                :index="child.path"
-                                v-for="(child,indexj) in item.children"
-                                :key="indexj"
-                            >
-                                {{child.name}}
-                            </el-menu-item>
-                        </el-submenu>
-                    </el-menu>
-                </el-aside>
-                <el-main>
-                    <el-breadcrumb
-                        separator-class="el-icon-arrow-right"
-                        v-if="this.$router.currentRoute.path!='/home'"
-                    >
-                        <el-breadcrumb-item :to="{ path: '/home' }">首页</el-breadcrumb-item>
-                        <el-breadcrumb-item>{{this.$router.currentRoute.name}}</el-breadcrumb-item>
-                    </el-breadcrumb>
-                    <div
-                        v-if="this.$router.currentRoute.path=='/home'"
-                    >
-                        系统主页
-                    </div>
-                    <router-view />
-                </el-main>
-            </el-container>
-        </el-container>
-    </div>
+  <el-container>
+    <el-header class="homeHeader">
+      <div class="title">广软宿舍管理系统</div>
+      <div>
+        <el-dropdown class="profile" @command="commandHandler">
+          <span class="el-dropdown-link">
+            {{user.name}}
+          </span>
+          <el-dropdown-menu slot="dropdown">
+            <el-dropdown-item command="profile">个人中心</el-dropdown-item>
+            <el-dropdown-item
+              command="logout"
+              divided
+            >注销登录</el-dropdown-item>
+          </el-dropdown-menu>
+        </el-dropdown>
+      </div>
+    </el-header>
+    <el-container>
+      <el-aside width="200px">
+        <el-menu
+          router
+          unique-opened
+        >
+          <el-submenu
+            :index="index+''"
+            v-for="(item,index) in routes"
+            v-if="!item.hidden"
+            :key="index"
+          >
+            <template slot="title">
+              <i
+                style="color: #409eff;margin-right: 5px"
+                :class="item.iconCls"
+              ></i>
+              <span>{{item.name}}</span>
+            </template>
+            <el-menu-item
+              :index="child.path"
+              v-for="(child,indexj) in item.children"
+              :key="indexj"
+            >
+              {{child.name}}
+            </el-menu-item>
+          </el-submenu>
+        </el-menu>
+      </el-aside>
+      <el-main>
+        <el-breadcrumb
+          separator-class="el-icon-arrow-right"
+          v-if="this.$router.currentRoute.path!='/home'"
+        >
+          <el-breadcrumb-item :to="{ path: '/home' }">首页</el-breadcrumb-item>
+          <el-breadcrumb-item>{{this.$router.currentRoute.name}}</el-breadcrumb-item>
+        </el-breadcrumb>
+        <div
+          class="homeWelcome"
+          v-if="this.$router.currentRoute.path=='/home'"
+        >
+          系统主页
+        </div>
+        <router-view class="homeRouterView" />
+      </el-main>
+    </el-container>
+  </el-container>
 </template>
 
 <script>
@@ -74,7 +71,7 @@ export default {
   name: "Home",
   data() {
     return {
-      user: JSON.parse(window.sessionStorage.getItem("user"))
+      // user: JSON.parse(window.sessionStorage.getItem("user"))
     };
   },
   computed: {
@@ -114,4 +111,41 @@ export default {
 </script>
 
 <style>
+.homeRouterView {
+  margin-top: 10px;
+}
+
+.homeWelcome {
+  text-align: center;
+  font-size: 30px;
+  font-family: 华文行楷;
+  color: #409eff;
+  padding-top: 50px;
+}
+
+.homeHeader {
+  background-color: #409eff;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 0px 15px;
+  box-sizing: border-box;
+}
+
+.homeHeader .title {
+  font-size: 30px;
+  font-family: 华文行楷;
+  color: #ffffff;
+}
+
+.homeHeader .profile {
+  font-size: 16px;
+  color: #ffffff;
+  cursor: pointer;
+}
+
+.el-dropdown-link {
+  display: flex;
+  align-items: center;
+}
 </style>
--- dorm-admin-page/src/views/Login.vue
+++ dorm-admin-page/src/views/Login.vue
@@ -1,107 +1,147 @@
 <template>
-    <div>
-        <img src="../assets/logo.png" alt="">
+    <div class="login">
+        <img
+            src="../assets/logo.png"
+            alt=""
+            style="margin-top:50px;"
+        >
         <el-form
-                :rules="rules"
-                ref="loginForm"
-                v-loading="loading"
-                element-loading-text="正在登录..."
-                element-loading-spinner="el-icon-loading"
-                element-loading-background="rgba(0, 0, 0, 0.8)"
-                :model="loginForm"
-                class="loginContainer">
+            :rules="rules"
+            ref="loginForm"
+            v-loading="loading"
+            element-loading-text="正在登录..."
+            element-loading-spinner="el-icon-loading"
+            element-loading-background="rgba(0, 0, 0, 0.8)"
+            :model="loginForm"
+            class="loginContainer"
+        >
             <h3 class="loginTitle">系统登录</h3>
             <el-form-item prop="username">
-                <el-input size="normal" type="text" v-model="loginForm.username" auto-complete="off"
-                          placeholder="请输入用户名"></el-input>
+                <el-input
+                    size="normal"
+                    type="text"
+                    v-model="loginForm.username"
+                    auto-complete="off"
+                    placeholder="请输入用户名"
+                ></el-input>
             </el-form-item>
             <el-form-item prop="password">
-                <el-input size="normal" type="password" v-model="loginForm.password" auto-complete="off"
-                          placeholder="请输入密码"></el-input>
+                <el-input
+                    size="normal"
+                    type="password"
+                    v-model="loginForm.password"
+                    auto-complete="off"
+                    placeholder="请输入密码"
+                ></el-input>
             </el-form-item>
             <el-form-item prop="code">
-                <el-input size="normal" type="text" v-model="loginForm.code" auto-complete="off"
-                          placeholder="点击图片更换验证码" @keydown.enter.native="submitLogin" style="width: 250px"></el-input>
-                <img :src="vcUrl" @click="updateVerifyCode" alt="" style="cursor: pointer">
+                <el-input
+                    size="normal"
+                    type="text"
+                    v-model="loginForm.code"
+                    auto-complete="off"
+                    placeholder="点击图片更换验证码"
+                    @keydown.enter.native="submitLogin"
+                    style="width: 250px"
+                ></el-input>
+                <img
+                    :src="vcUrl"
+                    @click="updateVerifyCode"
+                    alt=""
+                    style="cursor: pointer"
+                >
             </el-form-item>
-            <el-button size="normal" type="primary" style="width: 100%;" @click="submitLogin">登录</el-button>
+            <el-button
+                size="normal"
+                type="primary"
+                style="width: 100%;"
+                @click="submitLogin"
+            >登录</el-button>
         </el-form>
     </div>
 </template>
 
 <script>
-
-    export default {
-        name: "Login",
-        data() {
-            return {
-                loading: false,
-                vcUrl: '/api/verifyCode?time='+new Date(),
-                loginForm: {
-                    username: '',
-                    password: '',
-                    code:''
-                },
-                checked: true,
-                rules: {
-                    username: [{required: true, message: '请输入用户名', trigger: 'blur'}],
-                    password: [{required: true, message: '请输入密码', trigger: 'blur'}],
-                    code: [{required: true, message: '请输入验证码', trigger: 'blur'}]
-                }
+export default {
+  name: "Login",
+  data() {
+    return {
+      loading: false,
+      vcUrl: "/api/verifyCode?time=" + new Date(),
+      loginForm: {
+        username: "",
+        password: "",
+        code: ""
+      },
+      checked: true,
+      rules: {
+        username: [
+          { required: true, message: "请输入用户名", trigger: "blur" }
+        ],
+        password: [{ required: true, message: "请输入密码", trigger: "blur" }],
+        code: [{ required: true, message: "请输入验证码", trigger: "blur" }]
+      }
+    };
+  },
+  methods: {
+    updateVerifyCode() {
+      this.vcUrl = "/api/verifyCode?time=" + new Date();
+    },
+    submitLogin() {
+      this.$refs.loginForm.validate(valid => {
+        if (valid) {
+          this.loading = true;
+          this.postRequest("/api/doLogin", this.loginForm).then(resp => {
+            this.loading = false;
+            if (resp) {
+              this.$store.commit("INIT_CURRENTUSER", resp.obj);
+              window.sessionStorage.setItem("user", JSON.stringify(resp.obj));
+              let path = this.$route.query.redirect;
+              this.$router.replace(
+                path == "/" || path == undefined ? "/home" : path
+              );
+            } else {
+              this.vcUrl = "/api/verifyCode?time=" + new Date();
             }
-        },
-        methods: {
-            updateVerifyCode() {
-                this.vcUrl = '/api/verifyCode?time='+new Date();
-            },
-            submitLogin() {
-                this.$refs.loginForm.validate((valid) => {
-                    if (valid) {
-                        this.loading = true;
-                        this.postRequest('/api/doLogin', this.loginForm).then(resp => {
-                            this.loading = false;
-                            if (resp) {
-                                this.$store.commit('INIT_CURRENTUSER', resp.obj);
-                                window.sessionStorage.setItem("user", JSON.stringify(resp.obj));
-                                let path = this.$route.query.redirect;
-                                this.$router.replace((path == '/' || path == undefined) ? '/home' : path);
-                            }else{
-                                this.vcUrl = '/api/verifyCode?time='+new Date();
-                            }
-                        })
-                    } else {
-                        return false;
-                    }
-                });
-            }
+          });
+        } else {
+          return false;
         }
+      });
     }
+  }
+};
 </script>
 
 <style>
-    .loginContainer {
-        border-radius: 15px;
-        background-clip: padding-box;
-        margin: 180px auto;
-        width: 350px;
-        padding: 15px 35px 15px 35px;
-        background: #fff;
-        border: 1px solid #eaeaea;
-        box-shadow: 0 0 25px #cac6c6;
-    }
+.loginContainer {
+  border-radius: 15px;
+  background-clip: padding-box;
+  margin: 80px auto;
+  width: 350px;
+  padding: 15px 35px 15px 35px;
+  background: #fff;
+  border: 1px solid #eaeaea;
+  box-shadow: 0 0 25px #cac6c6;
+}
 
-    .loginTitle {
-        margin: 15px auto 20px auto;
-        text-align: center;
-        color: #505458;
-    }
+.loginTitle {
+  margin: 15px auto 20px auto;
+  text-align: center;
+  color: #505458;
+}
 
-    .loginRemember {
-        text-align: left;
-        margin: 0px 0px 15px 0px;
-    }
-    .el-form-item__content{
-        display: flex;
-        align-items: center;
-    }
+.loginRemember {
+  text-align: left;
+  margin: 0px 0px 15px 0px;
+}
+.el-form-item__content {
+  display: flex;
+  align-items: center;
+}
+.login {
+  display: flex;
+  align-items: center;
+  flex-direction:column;
+}
 </style>
