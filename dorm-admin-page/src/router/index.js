5cae3eec62314be977a79d2ced2ea9080ea79b8b
MxxxL <49240218+MxxxL@users.noreply.github.com>
2021-06-22 16:22:21 +0800

    dorm-admin

--- dorm-admin-page/src/router/index.js
+++ dorm-admin-page/src/router/index.js
@@ -1,17 +1,36 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-
+import Home from '../views/Home.vue'
 import Login from '../views/Login.vue'
+import Profile from '../views/Profile.vue'
 
 Vue.use(Router)
 
 export default new Router({
-  routes: [
-    {
-      path:'/',
+  routes: [{
+      path: '/',
       name: 'Login',
-      component:Login,
-      hidden:true
+      component: Login,
+      hidden: true
+    },
+    {
+      path: '/home',
+      name: 'Home',
+      component: Home,
+      hidden: true,
+      meta: {
+        roles: ['admin', 'user']
+      },
+      children: [{
+        path: '/profile',
+        name: 'Profile',
+        component: Profile,
+        hidden: true
+      }]
+    },
+    {
+      path: '*',
+      redirect: '/home'
     }
   ]
 })
